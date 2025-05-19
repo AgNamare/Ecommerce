67dd7ae27be0c6aaad479cd2d5b41fee6604c47d
Augustine Namare <agNamare@gmail.com>
2025-05-19 17:05:52 +0300

    Deploy admin dashboard

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client.js";
+import ReactDOM from "react-dom/client";
 import AppRoutes from "./AppRoutes.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "sonner";
