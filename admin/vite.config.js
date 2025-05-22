fd5110b1a914124b20d633106499f24ba20afa6d
Augustine Namare <agNamare@gmail.com>
2025-05-22 12:07:48 +0300

    Fix deployment errors

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -9,9 +9,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-   build: {
-    outDir: '../public/admin',
-    emptyOutDir: true
-  },
+  }
 });
--- client/vite.config.js
+++ client/vite.config.js
@@ -10,8 +10,4 @@ export default defineConfig({
     },
   },
   base: '/app/',  
-  build: {
-    outDir: '../public/app',
-    emptyOutDir: true
-  },
 });
