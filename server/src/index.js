dbb5817b01f391472ce42833f975fe43f331f7d5
Augustine Namare <agNamare@gmail.com>
2025-05-23 21:19:58 +0300

    Fix Deployment errors

--- server/src/index.js
+++ server/src/index.js
@@ -95,7 +95,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
