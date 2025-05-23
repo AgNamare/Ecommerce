c2960256fb66f5ebd0ebc545d955d700f44994ac
Augustine Namare <agNamare@gmail.com>
2025-05-23 14:14:21 +0300

    FIx deployment errors

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/admin/", 
+  base: "/admin/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-   build: {
-    outDir: '../server/public/admin',
-    emptyOutDir: true
+  build: {
+    outDir: "../server/public/admin",
+    emptyOutDir: true,
   },
 });
--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/',  
+  base: '/app/',  
   build: {
     outDir: '../server/public/app',
     emptyOutDir: true
